48588e8023b2d12286f2885485d5424b4dc3667f
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2020-10-27 00:09:35 +0100

    Fix domain with no unionOf

--- src/main/java/services/dataclasses/OntologyConcept.java
+++ src/main/java/services/dataclasses/OntologyConcept.java
@@ -1,8 +1,10 @@
 package services.dataclasses;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.AddAxiom;
@@ -17,12 +19,15 @@ import org.semanticweb.owlapi.model.OWLDataProperty;
 import org.semanticweb.owlapi.model.OWLDataPropertyDomainAxiom;
 import org.semanticweb.owlapi.model.OWLDataPropertyExpression;
 import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLDataUnionOf;
+import org.semanticweb.owlapi.model.OWLDataVisitor;
 import org.semanticweb.owlapi.model.OWLDatatype;
 import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
 import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLObjectUnionOf;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLPropertyExpression;
@@ -39,10 +44,13 @@ public class OntologyConcept {
     // All fields except "name" are optional. Look at schema-org.owl for examples.
     public String name = ""; // rdfs:label
     public String description = ""; // rdfs:comment
-    public String subClassof = ""; // F.eks. a AboutPage is a part of a WebPage, a brother is a part of a family (Merk: is a part of)
-    public String domain = ""; // F.eks. a truck is a type of car (Merk: is a type of)
-    public String range = ""; // F.eks. a car is a thing, a nose is a thing (Merk: is a)
-    public Boolean matchThis = true; // Typically you don't want to use the domains/range in the matching
+    public String domain = ""; // Same as subclassof, but for a header not a class, which means it wont be used
+                               // for the matching.
+
+    // Ask if you need to use these.
+    public String subClassof = ""; // F.eks. a AboutPage is a part of a WebPage, a brother is a part of a family
+                                   // (Merk: is a part of)
+    public String range = ""; // What kind of thing is it (A athlete is a person... etc)
 
     public static void main(String[] args) throws Exception {
         OWLOntologyManager m = OWLManager.createOWLOntologyManager();
@@ -59,22 +67,33 @@ public class OntologyConcept {
         OWLDataFactory df = OWLManager.getOWLDataFactory();
 
         Map<String, OWLClass> nameToOWLClass = new HashMap<String, OWLClass>();
-        //Fix label and description
-        for(OntologyConcept concept : ontologyConcepts){
-            if (nameToOWLClass.containsKey(concept.name)){ //It only uses one of the comments when duplicate entries occours. Good enough?
+        // Fix label and description
+        for (OntologyConcept concept : ontologyConcepts) {
+            if (nameToOWLClass.containsKey(concept.name)) { // It only uses one of the comments when duplicate entries
+                                                            // occours. Good enough?
                 continue;
             }
             OWLClass c = df.getOWLClass(IRI.create(example_iri + "#" + concept.name));
             AddLabel(concept.name, c, df, m, o);
             AddDescription(concept.description, c, df, m, o);
             nameToOWLClass.put(concept.name, c);
-            if (!concept.domain.equals("") || !concept.range.equals("") || !concept.matchThis){
-                throw new NotImplementedError("domain, range and !matchThis are currently not supported");
+            if (!concept.range.equals("")) {
+                throw new NotImplementedError("range are currently not supported");
             }
         }
-        //Fix subclasses
-        for(OntologyConcept concept : ontologyConcepts){
-            if (concept.subClassof.equals("")){
+        // Fix domains
+        for (OntologyConcept concept : ontologyConcepts) {
+            if (concept.domain.equals("")) {
+                continue;
+            }
+            OWLClass c = nameToOWLClass.get(concept.name);
+            OWLClass c2 = nameToOWLClass.get(concept.domain);
+
+            AddDomain(c, c2, df, m, o);
+        }
+        // Fix subclasses
+        for (OntologyConcept concept : ontologyConcepts) {
+            if (concept.subClassof.equals("")) {
                 continue;
             }
             OWLClass c = nameToOWLClass.get(concept.name);
@@ -83,20 +102,31 @@ public class OntologyConcept {
         }
         return o;
     }
-    public static void AddLabel(String name, OWLClass c, OWLDataFactory df, OWLOntologyManager m, OWLOntology o){
+
+    public static void AddLabel(String name, OWLClass c, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
         OWLAnnotation labelAnno = df.getOWLAnnotation(df.getRDFSLabel(), df.getOWLLiteral(name));
         OWLAxiom label_ax = df.getOWLAnnotationAssertionAxiom(c.getIRI(), labelAnno);
         m.applyChange(new AddAxiom(o, label_ax));
     }
 
-    public static void AddDescription(String description, OWLClass c, OWLDataFactory df, OWLOntologyManager m, OWLOntology o){
-        if (description == ""){
+    public static void AddDescription(String description, OWLClass c, OWLDataFactory df, OWLOntologyManager m,
+            OWLOntology o) {
+        if (description == "") {
             return;
         }
-        OWLAnnotation commentAnno = df.getOWLAnnotation(df.getRDFSComment(),
-        df.getOWLLiteral(description, ""));
+        OWLAnnotation commentAnno = df.getOWLAnnotation(df.getRDFSComment(), df.getOWLLiteral(description, ""));
         OWLAxiom comment_ax = df.getOWLAnnotationAssertionAxiom(c.getIRI(), commentAnno);
         m.applyChange(new AddAxiom(o, comment_ax));
+    }
+
+    public static void AddDomain(OWLClass c, OWLClass c2, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
+        if (c2 == null){
+            System.out.println("Warning: Domain could not find parent class");
+            return;
+        }
+        OWLObjectPropertyExpression c_as_property = df.getOWLObjectProperty(c.getIRI());
+        OWLObjectPropertyDomainAxiom domain = df.getOWLObjectPropertyDomainAxiom(c_as_property, c2);
+        m.applyChange(new AddAxiom(o, domain));
     }
 
     public static void AddSubClassOf(OWLClass child, OWLClass parent, OWLDataFactory df, OWLOntologyManager m, OWLOntology o){
@@ -118,14 +148,11 @@ public class OntologyConcept {
         OWLDataFactory df = OWLManager.getOWLDataFactory();
         OWLClass baby = df.getOWLClass(IRI.create(example_iri + "#Baby"));
         OWLClass person = df.getOWLClass(IRI.create(example_iri + "#Person"));
-        
-        //PrefixManager pm= new DefaultPrefixManager("http://www.co-ode.org/ontologies/ont.owl#");
-        OWLObjectPropertyExpression man= df.getOWLObjectProperty(baby.getIRI());
-        //OWLDatatype dt = df.getOWLDatatype("xsd:string",pm);
-        OWLObjectPropertyDomainAxiom domain=df.getOWLObjectPropertyDomainAxiom(man, person);
-        //OWLObjectPropertyRangeAxiom range= df.getOWLObjectPropertyRangeAxiom(man, (OWLClassExpression) dt);
+        OWLClass child = df.getOWLClass(IRI.create(example_iri + "#Child"));
 
-        m.applyChange(new AddAxiom(o, domain));
+        OWLClassExpression firstRuleSet = df.getOWLObjectUnionOf(person, child);
+        OWLObjectPropertyExpression c_as_property = df.getOWLObjectProperty(baby.getIRI());
+        m.addAxiom(o, df.getOWLObjectPropertyDomainAxiom(c_as_property, firstRuleSet));
         return o;
     }
 }
