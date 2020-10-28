5a9ffaaa1c940289d0920c0129c7dcee00441655
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2020-10-28 14:36:07 +0100

    Describe variables in ontologyConcecpt

--- src/main/java/services/dataclasses/OntologyConcept.java
+++ src/main/java/services/dataclasses/OntologyConcept.java
@@ -69,13 +69,13 @@ public class OntologyConcept {
     //Create OWLClasses
     for (OntologyConcept concept : ontologyConcepts) {
 
-      OWLClass c = df.getOWLClass(IRI.create(base_iri + "#" + concept.name));
+      OWLClass owlclass = df.getOWLClass(IRI.create(base_iri + "#" + concept.name));
 
-      AddLabel(concept.name, c, df, m, o);
+      AddLabel(concept.name, owlclass, df, m, o);
       //Add description
       if (!concept.description.equals("")){
         if (!nameToOWLClass.containsKey(concept.name)) { // It only uses one of the comments if there are duplicate entries
-          AddDescription(concept.description, c, df, m, o);
+          AddDescription(concept.description, owlclass, df, m, o);
         }
       }
       //Add range
@@ -93,32 +93,32 @@ public class OntologyConcept {
         domains.get(concept.name).add(concept.domain);
       }
 
-      nameToOWLClass.put(concept.name, c);
+      nameToOWLClass.put(concept.name, owlclass);
     }
     //Add ranges from map
     for (String key : ranges.keySet()){
-      OWLClass c = nameToOWLClass.get(key);
+      OWLClass owlclass = nameToOWLClass.get(key);
       //If all ranges references to classes, then its a objectProperty
       if (ranges.get(key).stream().allMatch(a -> nameToOWLClass.containsKey(a))){
         objectProperties.add(key);
       }
       if (objectProperties.contains(key)){
-        Set<OWLClass> c2 = ranges.get(key).stream().map(a -> nameToOWLClass.get(a)).collect(Collectors.toSet());
-        AddRangeObject(c, c2, df, m, o);
+        Set<OWLClass> rangeclasses = ranges.get(key).stream().map(a -> nameToOWLClass.get(a)).collect(Collectors.toSet());
+        AddRangeObject(owlclass, rangeclasses, df, m, o);
       } else {
         //Can't use unionOf with data type ranges. So there better be no duplicate entries
-        String range = ranges.get(key).iterator().next();
-        AddRangeData(c, range, df, m, o);
+        String rangedata = ranges.get(key).iterator().next();
+        AddRangeData(owlclass, rangedata, df, m, o);
       }
     }
     //Add domains from map
     for (String key : domains.keySet()){
-      OWLClass c = nameToOWLClass.get(key);
-      Set<OWLClass> c2 = domains.get(key).stream().map(a -> nameToOWLClass.get(a)).collect(Collectors.toSet());
+      OWLClass owlclass = nameToOWLClass.get(key);
+      Set<OWLClass> domainclasses = domains.get(key).stream().map(a -> nameToOWLClass.get(a)).collect(Collectors.toSet());
       if (objectProperties.contains(key)){
-        AddDomainObject(c, c2, df, m, o);
+        AddDomainObject(owlclass, domainclasses, df, m, o);
       } else {
-        AddDomainData(c, c2, df, m, o);
+        AddDomainData(owlclass, domainclasses, df, m, o);
       }
     }
     // Unused for now: Add subClassOf (duplicate/unionOf subclass entries are not supported)
@@ -126,72 +126,72 @@ public class OntologyConcept {
       if (concept.subClassof.equals("")) {
         continue;
       }
-      OWLClass c = nameToOWLClass.get(concept.name);
-      OWLClass c2 = nameToOWLClass.get(concept.subClassof);
-      AddSubClassOf(c, c2, df, m, o);
+      OWLClass owlclass = nameToOWLClass.get(concept.name);
+      OWLClass superclass = nameToOWLClass.get(concept.subClassof);
+      AddSubClassOf(owlclass, superclass, df, m, o);
     }
     return o;
   }
 
-  private static void AddLabel(String name, OWLClass c, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
+  private static void AddLabel(String name, OWLClass owlclass, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
     OWLAnnotation labelAnno = df.getOWLAnnotation(df.getRDFSLabel(), df.getOWLLiteral(name));
-    OWLAxiom label_ax = df.getOWLAnnotationAssertionAxiom(c.getIRI(), labelAnno);
+    OWLAxiom label_ax = df.getOWLAnnotationAssertionAxiom(owlclass.getIRI(), labelAnno);
     m.addAxiom(o, label_ax);
   }
 
-  private static void AddDescription(String description, OWLClass c, OWLDataFactory df, OWLOntologyManager m,
+  private static void AddDescription(String description, OWLClass owlclass, OWLDataFactory df, OWLOntologyManager m,
       OWLOntology o) {
     OWLAnnotation commentAnno = df.getOWLAnnotation(df.getRDFSComment(), df.getOWLLiteral(description, ""));
-    OWLAxiom comment_ax = df.getOWLAnnotationAssertionAxiom(c.getIRI(), commentAnno);
+    OWLAxiom comment_ax = df.getOWLAnnotationAssertionAxiom(owlclass.getIRI(), commentAnno);
     m.addAxiom(o, comment_ax);
   }
 
-  private static void AddDomainData(OWLClass c, Set<OWLClass> c2, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
-    OWLDataPropertyExpression c_as_property = df.getOWLDataProperty(c.getIRI());
+  private static void AddDomainData(OWLClass owlclass, Set<OWLClass> domainclasses, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
+    OWLDataPropertyExpression c_as_property = df.getOWLDataProperty(owlclass.getIRI());
     OWLDataPropertyDomainAxiom domain_ax = null;
-    if (c2.size() == 1){
-      domain_ax = df.getOWLDataPropertyDomainAxiom(c_as_property, c2.iterator().next());
+    if (domainclasses.size() == 1){
+      domain_ax = df.getOWLDataPropertyDomainAxiom(c_as_property, domainclasses.iterator().next());
     } else {
-      domain_ax = df.getOWLDataPropertyDomainAxiom(c_as_property, df.getOWLObjectUnionOf(c2));
+      domain_ax = df.getOWLDataPropertyDomainAxiom(c_as_property, df.getOWLObjectUnionOf(domainclasses));
     }
     m.addAxiom(o, domain_ax);
   }
-  private static void AddDomainObject(OWLClass c, Set<OWLClass> c2, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
-    OWLObjectPropertyExpression c_as_property = df.getOWLObjectProperty(c.getIRI());
+  private static void AddDomainObject(OWLClass owlclass, Set<OWLClass> domainclasses, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
+    OWLObjectPropertyExpression c_as_property = df.getOWLObjectProperty(owlclass.getIRI());
     OWLObjectPropertyDomainAxiom domain_ax = null;
-    if (c2.size() == 1){
-      domain_ax = df.getOWLObjectPropertyDomainAxiom(c_as_property, c2.iterator().next());
+    if (domainclasses.size() == 1){
+      domain_ax = df.getOWLObjectPropertyDomainAxiom(c_as_property, domainclasses.iterator().next());
     } else {
-      domain_ax = df.getOWLObjectPropertyDomainAxiom(c_as_property, df.getOWLObjectUnionOf(c2));
+      domain_ax = df.getOWLObjectPropertyDomainAxiom(c_as_property, df.getOWLObjectUnionOf(domainclasses));
     }
     m.addAxiom(o, domain_ax);
   }
 
-  private static void AddRangeData(OWLClass c, String range, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
-    OWLDataProperty c_as_property  = df.getOWLDataProperty(c.getIRI());
+  private static void AddRangeData(OWLClass owlclass, String rangedata, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
+    OWLDataProperty c_as_property  = df.getOWLDataProperty(owlclass.getIRI());
     PrefixManager pm = new DefaultPrefixManager(
                 "http://www.w3.org/2001/XMLSchema#");
-    OWLDataPropertyRangeAxiom range_ax = df.getOWLDataPropertyRangeAxiom(c_as_property, df.getOWLDatatype(range, pm));
+    OWLDataPropertyRangeAxiom range_ax = df.getOWLDataPropertyRangeAxiom(c_as_property, df.getOWLDatatype(rangedata, pm));
     m.addAxiom(o, range_ax);
   }
 
-  private static void AddRangeObject(OWLClass c, Set<OWLClass> c2, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
-    OWLObjectProperty c_as_property  = df.getOWLObjectProperty(c.getIRI());
+  private static void AddRangeObject(OWLClass owlclass, Set<OWLClass> rangeclasses, OWLDataFactory df, OWLOntologyManager m, OWLOntology o) {
+    OWLObjectProperty c_as_property  = df.getOWLObjectProperty(owlclass.getIRI());
     OWLObjectPropertyRangeAxiom range_ax = null;
-    if (c2.size() == 1){
-      range_ax = df.getOWLObjectPropertyRangeAxiom(c_as_property, c2.iterator().next());
+    if (rangeclasses.size() == 1){
+      range_ax = df.getOWLObjectPropertyRangeAxiom(c_as_property, rangeclasses.iterator().next());
     } else {
-      range_ax = df.getOWLObjectPropertyRangeAxiom(c_as_property, df.getOWLObjectUnionOf(c2));
+      range_ax = df.getOWLObjectPropertyRangeAxiom(c_as_property, df.getOWLObjectUnionOf(rangeclasses));
     }
     m.addAxiom(o, range_ax);
   }
 
-  private static void AddSubClassOf(OWLClass child, OWLClass parent, OWLDataFactory df, OWLOntologyManager m, OWLOntology o){
-    if (parent == null){
-      System.out.println("Warning: SubClassOf could not find parent class");
+  private static void AddSubClassOf(OWLClass owlclass, OWLClass superclass, OWLDataFactory df, OWLOntologyManager m, OWLOntology o){
+    if (superclass == null){
+      System.out.println("Warning: SubClassOf could not find superclass");
       return;
     }
-    OWLSubClassOfAxiom sub_ax = df.getOWLSubClassOfAxiom(child, parent);
+    OWLSubClassOfAxiom sub_ax = df.getOWLSubClassOfAxiom(owlclass, superclass);
     m.addAxiom(o, sub_ax);
   }
 
